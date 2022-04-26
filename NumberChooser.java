2306013daa8e74eb02bad207fc9895a5792de110
Charlychee <charles.lychee@gmail.com>
2022-04-25 23:55:25 -0700

    Added Number Chooser files

---
+++ NumberChooser.java
@@ -0,0 +1,6 @@
+public class NumberChooser {
+    public static final int seven = 7;
+	public static int chooseSeven() {
+	    return seven;
+    }
+}
---
+++ NumberChooserTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class NumberChooserTest {
+    @Test
+	public void checkSeven() {
+	    assertEquals(8, NumberChooser.chooseSeven());
+	}
+}
